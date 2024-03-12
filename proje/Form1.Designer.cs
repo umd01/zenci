7119f6d65741dff003d1d11bbf0ebfaf7a4e125c
umd01 <125164985+umd01@users.noreply.github.com>
2024-03-12 22:24:58 +0300

    Add files via upload

---
+++ proje/Form1.Designer.cs
@@ -0,0 +1,386 @@
+﻿namespace proje
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.adTextbox = new System.Windows.Forms.TextBox();
+            this.soyadTextbox = new System.Windows.Forms.TextBox();
+            this.adresTextbox = new System.Windows.Forms.TextBox();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.listBox5 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.listBox6 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(30, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "T.C";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(30, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Ad";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(30, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 20);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Soyad";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(30, 137);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(120, 20);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Doğum Tarihi ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(30, 172);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(125, 20);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Yaşadığı Şehir";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(30, 209);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 20);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Adres";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(161, 174);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // adTextbox
+            // 
+            this.adTextbox.Location = new System.Drawing.Point(82, 66);
+            this.adTextbox.Name = "adTextbox";
+            this.adTextbox.Size = new System.Drawing.Size(100, 20);
+            this.adTextbox.TabIndex = 2;
+            // 
+            // soyadTextbox
+            // 
+            this.soyadTextbox.Location = new System.Drawing.Point(95, 104);
+            this.soyadTextbox.Name = "soyadTextbox";
+            this.soyadTextbox.Size = new System.Drawing.Size(100, 20);
+            this.soyadTextbox.TabIndex = 2;
+            // 
+            // adresTextbox
+            // 
+            this.adresTextbox.Location = new System.Drawing.Point(93, 208);
+            this.adresTextbox.Multiline = true;
+            this.adresTextbox.Name = "adresTextbox";
+            this.adresTextbox.Size = new System.Drawing.Size(100, 99);
+            this.adresTextbox.TabIndex = 3;
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(72, 29);
+            this.maskedTextBox1.Mask = "00000000000";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(83, 20);
+            this.maskedTextBox1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(34, 313);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(217, 66);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Kaydet";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(319, 89);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 234);
+            this.listBox1.TabIndex = 6;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // listBox2
+            // 
+            this.listBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(438, 89);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 234);
+            this.listBox2.TabIndex = 6;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // listBox3
+            // 
+            this.listBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(557, 89);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(120, 234);
+            this.listBox3.TabIndex = 6;
+            this.listBox3.SelectedIndexChanged += new System.EventHandler(this.listBox3_SelectedIndexChanged);
+            // 
+            // listBox4
+            // 
+            this.listBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.Location = new System.Drawing.Point(676, 89);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(120, 234);
+            this.listBox4.TabIndex = 6;
+            this.listBox4.SelectedIndexChanged += new System.EventHandler(this.listBox4_SelectedIndexChanged);
+            // 
+            // listBox5
+            // 
+            this.listBox5.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listBox5.FormattingEnabled = true;
+            this.listBox5.Location = new System.Drawing.Point(795, 89);
+            this.listBox5.Name = "listBox5";
+            this.listBox5.Size = new System.Drawing.Size(120, 234);
+            this.listBox5.TabIndex = 6;
+            this.listBox5.SelectedIndexChanged += new System.EventHandler(this.listBox5_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(319, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(588, 66);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Listele";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(348, 365);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(43, 24);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "T.C";
+            // 
+            // maskedTextBox2
+            // 
+            this.maskedTextBox2.Location = new System.Drawing.Point(151, 137);
+            this.maskedTextBox2.Mask = "00/00/0000";
+            this.maskedTextBox2.Name = "maskedTextBox2";
+            this.maskedTextBox2.Size = new System.Drawing.Size(65, 20);
+            this.maskedTextBox2.TabIndex = 8;
+            this.maskedTextBox2.ValidatingType = typeof(System.DateTime);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(397, 368);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(107, 20);
+            this.textBox1.TabIndex = 9;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(575, 367);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(43, 24);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "T.C";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(624, 370);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(107, 20);
+            this.textBox2.TabIndex = 9;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(383, 409);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(95, 31);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "TC Ara";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.Location = new System.Drawing.Point(624, 409);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(95, 31);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "TC Sil";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button5.Location = new System.Drawing.Point(513, 461);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(95, 31);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Güncelle";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // listBox6
+            // 
+            this.listBox6.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listBox6.FormattingEnabled = true;
+            this.listBox6.Location = new System.Drawing.Point(915, 89);
+            this.listBox6.Name = "listBox6";
+            this.listBox6.Size = new System.Drawing.Size(120, 234);
+            this.listBox6.TabIndex = 6;
+            this.listBox6.SelectedIndexChanged += new System.EventHandler(this.listBox6_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1043, 515);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.maskedTextBox2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.listBox6);
+            this.Controls.Add(this.listBox5);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.adresTextbox);
+            this.Controls.Add(this.soyadTextbox);
+            this.Controls.Add(this.adTextbox);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "a";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox adTextbox;
+        private System.Windows.Forms.TextBox soyadTextbox;
+        private System.Windows.Forms.TextBox adresTextbox;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.ListBox listBox5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ListBox listBox6;
+    }
+}
+
---
+++ proje/Form1.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proje
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        ArrayList tckımlık = new ArrayList();
+        ArrayList adi = new ArrayList();
+        ArrayList soyadi = new ArrayList();
+        ArrayList dogumt = new ArrayList();
+        ArrayList sehir = new ArrayList();
+        ArrayList adres = new ArrayList();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Text = "Şehir Seçiniz";
+            comboBox1.Items.Add("Ankara");
+            comboBox1.Items.Add("Ağrı");
+            comboBox1.Items.Add("Artvin");
+            comboBox1.Items.Add("Kilis");
+            comboBox1.Items.Add("Osmaniye");
+            comboBox1.Items.Add("Düzce");
+            comboBox1.Items.Add("Hakkari");
+            comboBox1.Items.Add("Adana");
+            comboBox1.Items.Add("Hatay");
+            comboBox1.Items.Add("Gaziantep");
+            comboBox1.Items.Add("Kars");
+            comboBox1.Items.Add("Sakarya");
+
+
+
+        }
+
+             private void button1_Click(object sender, EventArgs e)
+             {
+               if (maskedTextBox1.Text == null || adTextbox.Text == null || soyadTextbox.Text == null || adresTextbox.Text==null || maskedTextBox2.Text==null)
+
+               {
+                MessageBox.Show("Bir yeri boş bıraktınız!")
+                    ;
+                
+                }
+                else
+                {
+                tckımlık.Add(maskedTextBox1.Text);
+                adi.Add(adTextbox.Text);
+                soyadi.Add(soyadTextbox.Text);
+                dogumt.Add(maskedTextBox2.Text);
+                sehir.Add(comboBox1.SelectedItem);
+                adres.Add(adresTextbox.Text);
+
+                maskedTextBox1.Clear();
+                adTextbox.Clear();
+                soyadTextbox.Clear();
+                maskedTextBox2.Clear();
+                comboBox1.Text = "Şehir Seçin";
+                adresTextbox.Clear();
+
+                maskedTextBox1.Select();
+                
+             }
+
+       
+             }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int sec = listBox1.SelectedIndex;
+            listBox2.SelectedIndex = sec;
+            listBox3.SelectedIndex = sec;
+            listBox4.SelectedIndex = sec;
+            listBox5.SelectedIndex = sec;
+            listBox6.SelectedIndex = sec;
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int sec = listBox2.SelectedIndex;
+            listBox1.SelectedIndex = sec;
+            listBox3.SelectedIndex = sec;
+            listBox4.SelectedIndex = sec;
+            listBox5.SelectedIndex = sec;
+            listBox6.SelectedIndex = sec;
+        }
+
+        private void listBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int sec = listBox3.SelectedIndex;
+            listBox1.SelectedIndex = sec;
+            listBox2.SelectedIndex = sec;
+            listBox4.SelectedIndex = sec;
+            listBox5.SelectedIndex = sec;
+            listBox6.SelectedIndex = sec;
+        }
+
+        private void listBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int sec = listBox4.SelectedIndex;
+            listBox1.SelectedIndex = sec;
+            listBox2.SelectedIndex = sec;
+            listBox3.SelectedIndex = sec;
+            listBox5.SelectedIndex = sec;
+            listBox6.SelectedIndex = sec;
+        }
+
+        private void listBox5_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int sec = listBox6.SelectedIndex;
+            listBox1.SelectedIndex = sec;
+            listBox2.SelectedIndex = sec;
+            listBox3.SelectedIndex = sec;
+            listBox4.SelectedIndex = sec;
+            listBox5.SelectedIndex = sec;
+        }
+
+        private void listBox6_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        int ind;
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            ind = 0;
+
+            string bulunacakTc = textBox1.Text;
+            bool bulundu = false;
+
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                string tcc = listBox1.Items[i].ToString();
+
+                if (tcc == bulunacakTc)
+                {
+                    ind = i;
+                    bulundu = true;
+                    break;
+                }
+            }
+
+            if (bulundu == true)
+            {
+                listBox1.Items.Clear();
+                listBox2.Items.Clear();
+                listBox3.Items.Clear();
+                listBox4.Items.Clear();
+                listBox5.Items.Clear();
+                listBox6.Items.Clear();
+
+                listBox1.Items.Add(tckımlık[ind]);
+                listBox2.Items.Add(adi[ind]);
+                listBox3.Items.Add(soyadi[ind]);
+                listBox4.Items.Add(dogumt[ind]);
+                listBox5.Items.Add(sehir[ind]);
+                listBox6.Items.Add(adres[ind]);
+            }
+            else
+            {
+                MessageBox.Show("Aradığınız TC Bulunamadı";
+            }
+            textBox1.Clear();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        { 
+            ind = 0;
+            string bulunacakTc = textBox2.Text;
+            bool bulundu = false;
+
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                string tcc = listBox1.Items[i].ToString();
+
+                if (tcc == bulunacakTc)
+                {
+                    ind = i;
+                    bulundu = true;
+
+                    break;
+                }
+            }
+
+            if (bulundu == true)
+            {
+                tckımlık.RemoveAt(ind);
+                adi.RemoveAt(ind);
+                soyadi.RemoveAt(ind);
+                dogumt.RemoveAt(ind);
+                sehir.RemoveAt(ind);
+                adres.RemoveAt(ind);
+
+               
+            }
+            else
+            {
+                
+            }
+            textBox2.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear(); listBox2.Items.Clear(); listBox3.Items.Clear(); listBox4.Items.Clear();
+            listBox5.Items.Clear(); listBox6.Items.Clear();
+
+            foreach (string adlar in adi)
+            {
+                listBox2.Items.Add(adlar);
+            }
+            foreach (string tcler in tckımlık)
+            {
+                listBox1.Items.Add(tcler);
+            }
+            foreach (string soyad in soyadi)
+            {
+                listBox3.Items.Add(soyad);
+            }
+            foreach (string dogumtarihi in dogumt)
+            {
+                listBox4.Items.Add(dogumtarihi);
+            }
+            foreach (string sehirler in sehir)
+            {
+                listBox5.Items.Add(sehirler);
+            }
+            foreach (string adresler in adres)
+            {
+                listBox6.Items.Add(adresler);
+            }
+        }
+    }
+
+}
